beac235aa58eab55db1a503a89278fb97ceb0237
Abdul Hameed <ahameed@redhat.com>
2023-11-29 09:28:01 -0500

    add config file for mta plugin update report url to set dynamically base on url defined on app-config.yaml file code cleanup and refactor
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>

--- plugins/mta/src/components/ApplicationInventory  /ApplicationInventory.tsx
+++ plugins/mta/src/components/ApplicationInventory  /ApplicationInventory.tsx
@@ -1,41 +1,23 @@
 import React from 'react';
-import useAsync from 'react-use/lib/useAsync';
 
-import { Link, Table, TableColumn } from '@backstage/core-components';
-import { useApi } from '@backstage/core-plugin-api';
+import { Link, Progress, Table, TableColumn } from '@backstage/core-components';
 
-import { Box, Grid, Typography } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 
-import { mtaApiRef } from '../../api';
-import { Application } from '../../types';
+import { useApplications } from '../../hooks';
+import { columns, useStyles } from './tableHeading';
 
 export const ApplicationInventory = () => {
-  const mtaClient = useApi(mtaApiRef);
+  const classes = useStyles();
 
-  const { loading: isApplicationsLoading, value: applicationData } =
-    useAsync(async (): Promise<Application[]> => {
-      return await mtaClient.getApplications();
-    }, []);
-
-  const Columns: TableColumn[] = [
-    { title: 'Name', field: 'name', highlight: true },
-    { title: 'Description', field: 'description', highlight: true },
-    { title: 'Business Service', field: 'businessService', highlight: true },
-    { title: 'Repository Kind', field: 'repository.kind', highlight: true },
-    { title: 'Repository URL', field: 'repository.url', highlight: true },
-    { title: 'Repository Branch', field: 'repository.branch', highlight: true },
-    { title: 'Repository Path', field: 'repository.path', highlight: true },
-    { title: 'Creating User', field: 'createUser', highlight: true },
-    {
-      title: 'Report',
-      field: 'report',
-      highlight: true,
-      render: (rowData: any): React.ReactNode => {
-        const app = rowData as Application;
-        return <Link to={`${app.report}`}>Report</Link>;
-      },
-    },
-  ];
+  const { isApplicationsLoading, applicationData } = useApplications();
+  if (isApplicationsLoading) {
+    return (
+      <div data-testid="mta-application-progress">
+        <Progress />
+      </div>
+    );
+  }
 
   return (
     <>
@@ -43,7 +25,7 @@ export const ApplicationInventory = () => {
         <Grid item xs={10}>
           <Table
             title="Application Inventory"
-            columns={Columns}
+            columns={columns}
             isLoading={isApplicationsLoading}
             data={applicationData || []}
             options={{
@@ -53,9 +35,10 @@ export const ApplicationInventory = () => {
               search: false,
             }}
             emptyContent={
-              <Box style={{ textAlign: 'center', padding: '15px' }}>
-                <Typography variant="body1">Backend data NOT found</Typography>
-              </Box>
+              <div className={classes.empty}>
+                No data was added yet,&nbsp;
+                <Link to="https://backstage.io/">learn how to add data</Link>.
+              </div>
             }
           />
         </Grid>
